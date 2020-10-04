27a12775117bd1ca667de9c64cd37fc129ab6204
samuelceron <samceronm@gmail.com>
2020-10-04 14:21:41 -0500

    Setting average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,11 +11,13 @@ min_change = 0
 previous = 0
 current_value = 0
 total_changes = 0
+total_in_changes = 0
 
 with open(budget_data_path,  "r", newline="") as budget_data:
     csv_reader  = csv.reader(budget_data, delimiter=",")
     csv_header = next(csv_reader)
-
+    first_row = next(csv_reader)
+    previous = first_row[1]
     for row in csv_reader:
         total_changes += 1
         total_months += 1
@@ -33,8 +35,10 @@ with open(budget_data_path,  "r", newline="") as budget_data:
         if (change < min_change):
             min_change = change
             min_date_change = row[0]
+        
+        total_in_changes = (total_in_changes) + change 
     
-
+average_change = round((float((total_in_changes)/total_changes)),2)
 print (total_months)
 print (total_budget)
 print (max_change)
@@ -43,12 +47,13 @@ print (total_changes)
 print (max_date_change)
 print (min_date_change)
 
+print(average_change)
 print(
    f"Financial Analysis\n"
    f"----------------------------\n"
    f"Total Months: {total_months}\n"
    f"Total: ${total_budget}\n"
-   f"Average  Change: 0\n"
+   f"Average  Change: {average_change}\n"
    f"Greatest Increase in Profits: {max_date_change} {max_change}\n"
    f"Greatest Decrease in Profits: {min_date_change} {min_change}\n"
     )
