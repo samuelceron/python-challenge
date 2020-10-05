b5d7263c3e2faa4754fee30f50fdd79de8308c57
samuelceron <samceronm@gmail.com>
2020-10-04 22:41:13 -0500

    Getting the candidates list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,12 +5,26 @@ election_data_path = os.path.join('Resources','election_data.csv')
 
 #Set variables and intialiazing 
 total_votes = 0
+candidates_list=[]
 
 with open(election_data_path, "r", newline="") as election_data:
     csv_reader = csv.reader(election_data, delimiter=",")
     csv_header = next(csv_reader)
 
+    #Getting the candidates list
     for row in csv_reader:
         total_votes += 1
+        if row[2] not in candidates_list:
+            candidates_list.append(row[2])
 
-print(total_votes)
+print(candidates_list)
+
+print(
+   f"Election Results\n"
+   f"----------------------------\n"
+   f"Total Votes: {total_votes}\n"
+   f"----------------------------\n"
+   f"Votes: \n"
+   f"----------------------------\n"
+   f"Winner: \n"
+    )
