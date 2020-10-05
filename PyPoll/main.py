b421c3b222ec5e23669103e562e921a56b9a6ac1
samuelceron <samceronm@gmail.com>
2020-10-05 00:29:34 -0500

    Calculating total, percent with election_data.csv and printing results in console

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,8 @@
 import os
 import csv
 
-election_data_path = os.path.join('Resources','test.csv')
+#election_data_path = os.path.join('Resources','test.csv')
+election_data_path = os.path.join('Resources','election_data.csv')
 
 #Set variables and intialiazing 
 total_votes = 0
@@ -25,20 +26,22 @@ with open(election_data_path, "r", newline="") as election_data:
    
     for candidate in candidates_list:
         candidate_total.append(votes_list.count(candidate))
-        percent_list.append(round(((votes_list.count(candidate))/(total_votes*100)),3))
+        percent_list.append(((votes_list.count(candidate))/(total_votes))*100)
 
-
+winner = candidates_list[candidate_total.index(max(candidate_total))]
 print(candidates_list)
 print(candidate_total)
-print(votes_list)
+#print(votes_list)
 print(percent_list)
 
 print(
    f"Election Results\n"
    f"----------------------------\n"
    f"Total Votes: {total_votes}\n"
-   f"----------------------------\n"
-   f"Votes: \n"
-   f"----------------------------\n"
-   f"Winner: \n"
+   f"----------------------------\n")
+for i in range(len(candidates_list)):
+        print(f'{candidates_list[i]}: {percent_list[i]}% ({candidate_total[i]})')
+print(
+    f"----------------------------\n"
+   f"Winner: {winner}\n"
     )
